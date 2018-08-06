49ad7e49faa49b7bc9b9e129df7d0eed51fa76d8
Andrew <Mclellan.an@gmail.com>
2018-08-06 11:03:01 -0400

    Add serializer to user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,11 @@
 class Api::V1::UsersController < ApplicationController
 
+  def index
+    render json: User.all
+  end
 
   def show
-    render json: { user: User.find(params[:id])}
+    render json: User.find(params[:id])
   end
-  
+
 end
--- app/javascript/react/components/UserShowTile.js
+++ app/javascript/react/components/UserShowTile.js
@@ -5,12 +5,18 @@ const UserShowTile = (props) => {
 
 
   return(
-    <div>
+    <div className="callout secondary">
+      <h4>First Name:</h4>
       {props.firstName}<br />
+      <h4>Last Name</h4>
       {props.lastName}<br />
+      <h4>Address: </h4>
       {props.address}<br />
+      <h4>City: </h4>
       {props.city}<br />
+      <h4>State: </h4>
       {props.state}<br/>
+      <h4>Zip Code: </h4>
       {props.zip}<br/>
     <div></div>
     </div>
