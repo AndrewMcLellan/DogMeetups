1d9bdcdbc8eef469e36069923ef275089ae44644
Andrew <Mclellan.an@gmail.com>
2018-08-10 12:06:57 -0400

    Finished Google Maps Rendering

--- app/javascript/react/components/MeetupTile.js
+++ app/javascript/react/components/MeetupTile.js
@@ -3,22 +3,28 @@ import MapContainer from '../containers/MapContainer'
 
 const MeetupTile = (props) => {
   return(
-    <div className="callout secondary">
-      {props.creator}
-      <br />
-      {props.location}
-      <div>
-        <MapContainer
-          latitude={props.lat}
-          longitude={props.lng}
-          />
-      </div>
-      <br />
-      {props.description}
-        <div>
+    <div>
+      <div className="callout secondary columns small-6 meetup_show_tile" id="meetup_show_tile">
+        <div className="callout">
+          <div>
+            {props.creator}
+            <br />
+            {props.location}
+          </div>
+          <br />
+          {props.description}
+        </div>
+        <div className="row column small-2">
+          <MapContainer
+            latitude={props.lat}
+            longitude={props.lng}
+            />
+        </div>
+        <div className="small-1">
           <form onSubmit={props.handleSubmit}>
             <button className="hover">Hello</button>
           </form>
+        </div>
         </div>
     </div>
   )
