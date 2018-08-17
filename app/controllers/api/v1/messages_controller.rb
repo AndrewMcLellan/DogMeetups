c2aa5ca7b850ef047560a644fafa117f2519f9a3
Andrew <Mclellan.an@gmail.com>
2018-08-17 13:40:58 -0400

    okay

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,3 +1,7 @@
 class Api::V1::MessagesController < ApplicationController
 
+  def index
+    render json: Message
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :messages
       resources :meetups do
         resources :attendances
+        resources :messages
       end
       post 'meetups/search', to: 'meetups#search'
     end
