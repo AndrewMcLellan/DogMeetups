beba561a1f510a83eada0d62c00306a5476cf146
Andrew <Mclellan.an@gmail.com>
2018-10-28 15:39:58 -0400

    L

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,28 @@
 class UsersController < ApplicationController
 
   def index
-    
+
   end
 
   def show
     @user = User.find(params[:id])
   end
 
-  # def edit
-  #   @user = User.find(params(:id))
-  # end
+  def edit
+    @user = User.find(params(:id))
+  end
 
-  # def update
-  #   @user = User.find(params[:id])
-  #   @user.update(user_params)
-  #   if @user.update
-  #     flash[:notice] = "Profile Updated Successfully"
-  #     redirect_to user_path(@user.id)
-  #   else
-  #     @errors = @user.errors.full_messages
-  #     render action: 'edit'
-  #   end
-  # end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.update
+      flash[:notice] = "Profile Updated Successfully"
+      redirect_to user_path(@user.id)
+    else
+      @errors = @user.errors.full_messages
+      render action: 'edit'
+    end
+  end
 
   private
 
